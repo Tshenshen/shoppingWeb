eea83ea404ecd3f57c8e3cb977277c9baf79f46f
tshen <814976777@qq.com>
2020-03-06 17:44:19 +0800

    商家注册功能

--- src/main/java/com/liang/shoppingweb/mapper/user/UserMapper.java
+++ src/main/java/com/liang/shoppingweb/mapper/user/UserMapper.java
@@ -14,7 +14,7 @@ public interface UserMapper {
     @Select("select * from tbl_user")
     List<User> getAll();
 
-    @Insert("insert into tbl_user(id,username,password,balance,email,role,enable,create_date) values(#{id},#{username},#{password},0,#{email},#{role},#{enable},#{createDate})")
+    @Insert("insert into tbl_user(id,username,password,balance,email,role,enable,create_date) values(#{id},#{username},#{password},0,#{email},'ROLE_USER',#{enable},#{createDate})")
     void insertUser(User user);
 
     @Update("update tbl_user set last_login_date = #{lastLoginDate} where username = #{username}")
