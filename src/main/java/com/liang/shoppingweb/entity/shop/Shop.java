4e1f5dcdfc651f2a3275282703d8661d24b4b143
tshen <814976777@qq.com>
2020-03-06 14:03:48 +0800

    goods模块改为shop模块

--- src/main/java/com/liang/shoppingweb/entity/shop/Shop.java
+++ src/main/java/com/liang/shoppingweb/entity/shop/Shop.java
@@ -1,4 +1,29 @@
 package com.liang.shoppingweb.entity.shop;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Shop {
+    /**
+     * 店铺id
+     */
+    private String id;
+    /**
+     * 商品id
+     */
+    private String goodsId;
+    /**
+     * 商家名
+     */
+    private String enterpriseName;
+    /**
+     * 创建时间
+     */
+    private Date createDate;
+    /**
+     * 修改时间
+     */
+    private Date updateDate;
 }
